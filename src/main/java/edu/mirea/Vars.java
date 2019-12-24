20422df2666f6127202c1c4bfc15071c35ada533
kolyanpie <k0lyan.pie@gmail.com>
2019-12-24 13:22:12 +0300

    add login

---
+++ src/main/java/edu/mirea/Vars.java
@@ -0,0 +1,8 @@
+package edu.mirea;
+
+import javafx.stage.Stage;
+
+public class Vars {
+    public static Stage stage;
+    public static boolean isAdmin;
+}
---
+++ src/main/java/edu/mirea/fxml/Login.java
@@ -0,0 +1,59 @@
+package edu.mirea.fxml;
+
+import edu.mirea.Vars;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+
+public class Login {
+    private Alert wrongData;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private PasswordField passField;
+
+    {
+        wrongData = new Alert(Alert.AlertType.ERROR);
+    }
+
+    @FXML
+    private void clickExit() {
+        System.exit(0);
+    }
+
+    @FXML
+    private void clickLogin() throws IOException {
+        if (login(loginField.getText(), passField.getText())) {
+            Parent parent = FXMLLoader.load(getClass().getResource("main.fxml"));
+            Vars.stage.setScene(new Scene(parent));
+        } else {
+            wrongData.setContentText("wrong login or pass");
+            wrongData.show();
+        }
+    }
+
+    private boolean login(String login, String pass) {
+        switch (login) {
+            case "admin":
+            case "root":
+            case "master":
+                Vars.isAdmin = true;
+                break;
+            case "user":
+            case "misha":
+            case "zhenya":
+                Vars.isAdmin = false;
+                break;
+            default:
+                return false;
+        }
+        return pass.equals(login + "123");
+    }
+}
